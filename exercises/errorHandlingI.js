b885e5dbfe9c6f96c485a86b6d6a708ebae60cb6
Bryce Adams <badams@apge.com>
2019-05-03 09:52:50 -0500

    Delete second Error handling challenge

--- exercises/errorHandlingI.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = function(url){
-/*
-	You are given a url. Make a GET call with axios using the given url. However, there is something wrong with the url, and it throws an error. This error will have a property 'message'. Throw the message.
-	
-	Write your code below the comment.
-*/
-
-};
